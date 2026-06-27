ff4b9cd34ebd320bc9a64da0b89ff0db44792c0a
ibnjemil <ibnjemil320@gmail.com>
2026-06-27 05:58:01 +0000

    lazy db init - defer env read to runtime

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,25 @@
 import { PrismaClient } from '@prisma/client'
 
-const dbUrl = process.env.ASMYA_DB_URL
-const { createClient } = require('@libsql/client')
-const { PrismaLibSQL } = require('@prisma/adapter-libsql')
-const libsql = createClient({ url: dbUrl })
-const adapter = new PrismaLibSQL(libsql)
+let _db: PrismaClient | undefined
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+function createDbClient(): PrismaClient {
+  const dbUrl = process.env.ASMYA_DB_URL
+  const { createClient } = require('@libsql/client')
+  const { PrismaLibSQL } = require('@prisma/adapter-libsql')
+  const libsql = createClient({ url: dbUrl })
+  const adapter = new PrismaLibSQL(libsql)
+  return new PrismaClient({ adapter })
 }
 
-export const db = globalForPrisma.prisma ?? new PrismaClient({ adapter })
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
-// v2
+function getDb(): PrismaClient {
+  if (!_db) {
+    _db = createDbClient()
+  }
+  return _db
+}
+
+export const db = new Proxy({} as PrismaClient, {
+  get(_, prop) {
+    return (getDb() as any)[prop]
+  },
+})
