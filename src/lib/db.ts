6f9d3162bd35993c07ba396a7cb61d25056aefaf
ibnjemil <ibnjemil320@gmail.com>
2026-06-28 08:31:19 +0000

    fix: adapter with DATABASE_URL set on Vercel

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
+
 let _db: PrismaClient | undefined;
+
 export const db = new Proxy({} as PrismaClient, {
   get(_, prop) {
-    if (!_db) _db = new PrismaClient();
+    if (!_db) {
+      const libsql = createClient({
+        url: process.env.ASMYA_DB_URL || "",
+        authToken: process.env.TURSO_AUTH_TOKEN,
+      });
+      _db = new PrismaClient({ adapter: new PrismaLibSql(libsql) });
+    }
     return (_db as any)[prop];
   }
 });
