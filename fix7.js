c6e202037c387a0a158c61f5f488d5cd69cdfebe
ibnjemil <ibnjemil320@Gmail.com>
2026-08-06 19:54:42 +0300

    fix: push build crash

---
+++ fix7.js
@@ -0,0 +1,6 @@
+﻿var fs = require('fs');
+var base = process.cwd();
+var f = fs.readFileSync(base + '/src/app/api/push/send/route.ts', 'utf8');
+f = f.replace(/const VAPID_PUBLIC_KEY = process\.env.*\n.*\n.*\n\nif \(VAPID_PUBLIC_KEY && VAPID_PRIVATE_KEY\) \{[\s\S]*?\}\n\n/, '');
+fs.writeFileSync(base + '/src/app/api/push/send/route.ts', f, 'utf8');
+console.log('DONE: removed top-level setVapidDetails (was crashing build)');
