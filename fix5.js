3ef0b728c729925326ed67ea9d86303e66d67eb2
ibnjemil <ibnjemil320@Gmail.com>
2026-08-06 19:39:28 +0300

    fix: add use client to banner

---
+++ fix5.js
@@ -0,0 +1,8 @@
+﻿var fs = require('fs');
+var f = fs.readFileSync('src/components/mesjid/UrgentPlanBanner.tsx', 'utf8');
+if (f.indexOf("'use client'") === -1) {
+  fs.writeFileSync('src/components/mesjid/UrgentPlanBanner.tsx', "'use client'\n" + f, 'utf8');
+  console.log('Added use client');
+} else {
+  console.log('Already has use client');
+}
